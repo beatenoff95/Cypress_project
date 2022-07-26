cb6d7f291b8fbdcf430c2b9eacce2088359387f2
beatenoff95 <beatenoff123321@gmail.com>
2022-07-26 15:06:16 +0300

    changes

--- cypress/e2e/test1/HoverOnElement.cy.js
+++ cypress/e2e/test1/HoverOnElement.cy.js
@@ -9,6 +9,20 @@ it('Hover On Elements', ()=> {
     .get('[class="figcaption"]') .eq(0) .invoke('show')
     .should('contains.text', 'name: user1')
     .should('contains.text', 'View profile')
+    .get('[href="/users/1"]')
+    .should('contains.text', 'View profile' )
+    .get('[class="figure"]') .eq(1) .trigger('mousehover') 
+    .get('[class="figcaption"]') .eq(1) .invoke('show')
+    .should('contains.text', 'name: user')
+    .should('contains.text', 'View profile')
+    .get('[href="/users/2"]')
+    .should('contains.text', 'View profile' )
+    .get('[class="figure"]') .eq(2) .trigger('mousehover') 
+    .get('[class="figcaption"]') .eq(2) .invoke('show')
+    .should('contains.text', 'name: user')
+    .should('contains.text', 'View profile')
+    .get('[href="/users/3"]')
+    .should('contains.text', 'View profile' )
 
 
    
