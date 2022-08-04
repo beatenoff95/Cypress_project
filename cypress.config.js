ecc9cef93274249a4c3dc0980ba62ade96eeef31
beatenoff95 <beatenoff123321@gmail.com>
2022-08-04 20:39:25 +0300

    commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,21 @@
-module.exports = {
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'cypress-mochawesome-reporter, mocha-junit-reporter',
+    mochaJunitReporterReporterOptions: {
+      mochaFile: 'cypress/reports/junit/results.xml',
+    },
+    cypressMochawesomeReporterReporterOptions: {
+      charts: true,
+      reportPageTitle: 'custom-title',
     },
   },
-};
-
-require('cypress-downloadfile/lib/downloadFileCommand')
+  video: false,
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+  },
+});
